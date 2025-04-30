100029330b37b7df23380a73dda687feab5d443f
FurioStyle <benjamin.bglh@gmail.com>
2025-04-30 00:17:14 -0400

    Clase abstracta para otras Monedas

--- Monedas/Moneda.java
+++ Monedas/Moneda.java
@@ -1,4 +1,14 @@
 package Monedas;
 
-abstract public class Moneda {
+public abstract class Moneda{
+    private int serie;
+
+    public Moneda(){
+    }
+
+    public abstract int getValor();
+
+    public Moneda getSerie() {
+        return this;
+    }
 }
