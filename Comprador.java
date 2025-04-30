34cb0090f7e1cec8c3b10f01c6d6d3c602c600ef
FurioStyle <benjamin.bglh@gmail.com>
2025-04-30 00:19:15 -0400

    codigo base para el comprador

--- Comprador.java
+++ Comprador.java
@@ -1,2 +1,28 @@
-public class Comprador {
+import Bebidas.Bebida;
+import Monedas.*;
+
+public class Comprador{
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, int cual, Expendedor exp){
+        Bebida b = exp.comprarBebida(m, cual);
+        if (b != null) {
+            this.sonido = b.beber();
+        }
+        Moneda moneda;
+        while ((moneda = exp.getVuelto()) != null) {
+            this.vuelto += moneda.getValor();
+        }
+    }
+
+    public int cuantoVuelto(){
+
+        return vuelto;
+    }
+
+    public String queBebiste(){
+        return sonido;
+    }
 }
+
