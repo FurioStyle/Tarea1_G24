4a8179df553d35e2bf113b8840bb56066903889c
FurioStyle <benjamin.bglh@gmail.com>
2025-04-30 00:16:33 -0400

    Clases para el deposito de monedas y el de productos

--- Deposito/Deposito.java
+++ Deposito/Deposito.java
@@ -1,4 +1,27 @@
 package Deposito;
 
+import Bebidas.*;
+
+import java.util.ArrayList;
+
 public class Deposito {
+    private ArrayList<Bebida> bebidas;
+
+    public Deposito() {
+        bebidas = new ArrayList();
+    }
+
+    public void addBebida(Bebida b) {
+        bebidas.add(b);
+    }
+
+    public Bebida getBebida() {
+        if (bebidas.size() != 0) {
+            Bebida aux = bebidas.get(0);
+            bebidas.remove(0);
+            return aux;
+        } else {
+            return null;
+        }
+    }
 }
