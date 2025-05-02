464db252c3880994ab97d469570c1829d10b6b99
FurioStyle <benjamin.bglh@gmail.com>
2025-05-02 14:30:46 -0400

    codigo actualizado del comprador

---
+++ Comprador.java
@@ -0,0 +1,29 @@
+import Productos.Bebida;
+import Monedas.*;
+import Productos.Producto;
+import Productos.ProductoEnum;
+
+public class Comprador{
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, ProductoEnum cual, Expendedor exp){
+        Producto b = exp.comprarProducto(m, cual);
+        if (b != null) {
+            this.sonido = b.consumir();
+        }
+        Moneda moneda;
+        while ((moneda = exp.getVuelto()) != null) {
+            this.vuelto += moneda.getValor();
+        }
+    }
+
+    public int cuantoVuelto() {
+        return vuelto;
+    }
+
+    public String queBebiste() {
+        return sonido;
+    }
+}
+
