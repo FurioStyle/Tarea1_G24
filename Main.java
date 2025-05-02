c40a8f7e127875ebdc20487a2356cae6ee56d211
FurioStyle <benjamin.bglh@gmail.com>
2025-05-02 14:51:58 -0400

    Quitado un array de prueba

--- Main.java
+++ Main.java
@@ -11,21 +11,7 @@ public class Main {
         m = new Moneda1000();
         ProductoEnum tipo = ProductoEnum.SNICKERS;
         c = new Comprador(m,tipo,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto());
-
-        ArrayList<Moneda> lista = new ArrayList<>();
-        lista.add(new Moneda500());
-        lista.add(new Moneda100());
-        lista.add(new Moneda1000());
-
-        Collections.sort(lista);
-
-        for (int i = 0; i < lista.size(); i++) {
-            Moneda money = lista.get(i);
-            System.out.println(money.getValor());
-        }
-
-
+        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
     }
 }
 
