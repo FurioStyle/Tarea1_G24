60b331193e123c93729f0f5f62601cde6a8fe29d
FurioStyle <benjamin.bglh@gmail.com>
2025-05-02 18:55:33 -0400

    feat: Pruebas con las exceptions

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import Excepciones.NoHayProductoException;
+import Excepciones.PagoIncorrectoException;
 import Monedas.*;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoHayProductoException, PagoIncorrectoException {
         Moneda m = null;
         Comprador c = null;
         m = new Moneda1000();
@@ -22,6 +24,23 @@ public class Main {
         // 5 = Snickers
         c = new Comprador(m, 5);
         System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+        // Excepciones
+
+        // No hay dinero suficiente
+
+        // m = new Moneda100();
+        // c = new Comprador(m,1);
+
+        // Numero Invalido
+
+        // m = new Moneda1000();
+        // c = new Comprador(m,7);
+
+        //Moneda Nula
+
+        // m = null;
+        // c = new Comprador(m,1);
     }
 }
 
